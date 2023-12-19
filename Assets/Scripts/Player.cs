37aad353bb18febd42e6d9c5dcd55043d099e6d0
Sebastian Lundgren <relinkz@users.noreply.github.com>
2023-12-19 16:54:11 +0100

    Add character sprite and animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,24 @@
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D m_rigidbody = null;
+    Animator m_animator = null;
+    SpriteRenderer m_spriteRenderer = null;
     
     [SerializeField] Vector2 m_appliedMoveForce;
     [SerializeField] Vector2 m_appliedJumpForce;
-
     [SerializeField] float m_maxSpeed;
+    [SerializeField] float m_inAirAcc;
+
+    bool m_inAir;
+    [SerializeField] bool m_leftAnim;
 	// Start is called before the first frame update
 	void Start()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     enum Direction
@@ -22,19 +27,31 @@ public class Player : MonoBehaviour
     }
     void HandleHorizonalMovement(Direction dir)
     {
-        Debug.Log(dir);
 		var velocity = m_rigidbody.velocity;
+
         float dirMod = (dir == Direction.Right) ? 1 : -1;
+        float inAirSlowModifier = (m_inAir == false) ? 1.0f : 0.25f;
+
+        m_inAir = false;
+        m_leftAnim = false;
+
 		if (velocity.magnitude < m_maxSpeed)
 		{
-			m_rigidbody.AddRelativeForce(m_appliedMoveForce * dirMod);
+			m_rigidbody.AddRelativeForce(m_appliedMoveForce * dirMod * inAirSlowModifier);
             return;
 		}
 	}
 
     void TryJump()
     {
+        if (m_inAir) 
+        { 
+            return; 
+        }
+
         m_rigidbody.AddRelativeForce(m_appliedJumpForce);
+
+        m_inAir = true;
     }
 
     void HandleInput()
@@ -57,5 +74,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         HandleInput();
+
+        var vel = m_rigidbody.velocity;
+		//m_leftAnim = vel.x < 0.0f;
+
+		m_animator.SetFloat("playerSpeed", m_rigidbody.velocity.magnitude);
+
+        m_spriteRenderer.flipX = m_leftAnim;
+        //m_animator.SetBool("left", m_leftAnim);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		m_inAir = false;
 	}
 }
